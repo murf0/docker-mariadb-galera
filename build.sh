c639a31a46941e961dd0f53c28f6ea9bab2c63b9
Mikael Murf Mellgren <mikael@murf.se>
2015-11-10 22:19:51 +0100

    Build works. Modified my.cnf as mariadb 10.1.1

--- build.sh
+++ build.sh
@@ -1,22 +1,24 @@
 # Set correct environment variables.
+set -ex
+
+export LC_ALL=C
+export DEBIAN_FRONTEND=noninteractive
+export TERM=dumb
 
 # WORKAROUND for docker build errors
 ln -s -f /bin/true /usr/bin/chfn
 
-LC_ALL=C
-DEBIAN_FRONTEND=noninteractive
-TERM=dumb
-
 # Install MariaDB
-apt-get -y update
 apt-get install -y software-properties-common
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+sudo add-apt-repository 'deb [arch=amd64,i386] http://ftp.ddg.lth.se/mariadb/repo/10.1/ubuntu trusty main'
+
 apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-add-apt-repository 'deb http://mirror.netcologne.de/mariadb/repo/10.1/ubuntu trusty main'
 add-apt-repository 'deb http://repo.percona.com/apt trusty main'
 apt-get -y update
+apt-get -y upgrade
 
-apt-get install -y iproute mariadb-galera-server galera-3 rsync netcat-openbsd socat pv iputils-ping net-tools percona-xtrabackup
+apt-get install -y iproute mariadb-server galera-3 pv iputils-ping net-tools percona-xtrabackup
 
 mv /usr/bin/xtrabackup /usr/bin/xtrabackup_51
 ln -sf /usr/bin/xtrabackup_55 /usr/bin/xtrabackup
