ed6bb8f624d43adfd1dc76b56604c68e7a0a25bb
Mikael Murf Mellgren <mikael@murf.se>
2015-11-10 23:05:54 +0100

    fized build and startup

--- mariadbgalera.sh
+++ mariadbgalera.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 T=$(curl -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" $TUTUM_SERVICE_API_URL | jq '.' | grep ENV_TUTUM_IP_ADDRESS | grep -v ${TUTUM_IP_ADDRESS} | awk -F\" '{print $4}' | awk -F\/ '{print $1}')
-CLUSTER="gcomm://"
+CLUSTER=""
 for Y in $T; do
     CLUSTER="${CLUSTER}${Y},"
 done
@@ -10,9 +10,9 @@ NODE_ADDR=$(echo ${TUTUM_IP_ADDRESS} | awk -F\/ '{print $1}')
 
 VOLUME_HOME="/var/lib/mysql"
 
-if [ "x${CLUSTER}" = "xgcomm://" ]; then
+if [ "x${CLUSTER}" = "x" ]; then
     echo "I'm alone Bootstrap Cluster (Throw away container if this is not the first container)"
-    if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    if [ ! -d $VOLUME_HOME/mysql ]; then
         mysql_install_db > /dev/null 2>&1
         /create_mariadb_admin_user.sh
     fi
@@ -20,8 +20,8 @@ if [ "x${CLUSTER}" = "xgcomm://" ]; then
         --wsrep_node_incoming_address="${NODE_ADDR}" \
         --wsrep_new_cluster --wsrep_cluster_address="gcomm://"
 else
-    echo "I'm not first! My buddies: $CLUSTER"
+    echo "I'm not first! My buddies: ${CLUSTER}"
     /usr/bin/mysqld_safe --wsrep_node_address="${NODE_ADDR}" \
         --wsrep_node_incoming_address="${NODE_ADDR}" \
-        --wsrep_cluster_address=${CLUSTER}
+        --wsrep_cluster_address="gcomm://${CLUSTER}"
 fi
