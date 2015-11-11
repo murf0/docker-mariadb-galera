70219be52cf60f0950de0479d94e35dfe5d3a4cb
Mikael Murf Mellgren <mikael@murf.se>
2015-11-11 14:25:34 +0100

    changed

--- mariadbgalera.sh
+++ mariadbgalera.sh
@@ -12,10 +12,11 @@ VOLUME_HOME="/var/lib/mysql"
 if [ "x${CLUSTER}" = "x" ]; then
     echo "I'm alone Bootstrap Cluster (Throw away container if this is not the first container)"
     if [ ! -d $VOLUME_HOME/mysql ]; then
+        echo "run mysql_install_db"
         mysql_install_db
         /create_mariadb_admin_user.sh
     fi
 else
-    echo "I'm not first! My buddies: ${CLUSTER}"
+    echo "I'm not alone! My buddies: ${CLUSTER}"
 fi
 /usr/bin/mysqld_safe --wsrep_node_address="${NODE_ADDR}" --wsrep_node_incoming_address="${NODE_ADDR}" --wsrep_cluster_address="gcomm://${CLUSTER}"
