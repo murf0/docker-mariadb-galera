b85847233f3db129237fb93c7175e840af33c695
Mikael Murf Mellgren <mikael@murf.se>
2015-11-11 18:52:02 +0100

    erroor in gcomm

--- mariadbgalera.sh
+++ mariadbgalera.sh
@@ -11,7 +11,10 @@ VOLUME_HOME="/var/lib/mysql"
 
 if [ "x${CLUSTER}" = "x" ]; then
     echo "I'm alone Bootstrap Cluster (Throw away container if this is not the first container)"
+    CLUSTER="gcomm://"
 else
     echo "I'm not alone! My buddies: ${CLUSTER}"
+    CLUSTER="gcomm://${CLUSTER}"
 fi
-/usr/bin/mysqld_safe --wsrep_node_address="${NODE_ADDR}" --wsrep_node_incoming_address="${NODE_ADDR}" --wsrep_cluster_address="gcomm://${CLUSTER}"
+echo /usr/bin/mysqld_safe --wsrep_node_address="${NODE_ADDR}" --wsrep_node_incoming_address="${NODE_ADDR}" --wsrep_cluster_address="${CLUSTER}"
+/usr/bin/mysqld_safe --wsrep_node_address="${NODE_ADDR}" --wsrep_node_incoming_address="${NODE_ADDR}" --wsrep_cluster_address="${CLUSTER}"
