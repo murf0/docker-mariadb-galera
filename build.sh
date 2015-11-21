72e0f3f7bcf637fb1a0b2a7d0bed59bd22a553f2
Mikael Murf Mellgren <mikael@murf.se>
2015-11-21 01:50:10 +0100

    Removed old mariadb setup

--- build.sh
+++ build.sh
@@ -9,14 +9,13 @@ export TERM=dumb
 ln -s -f /bin/true /usr/bin/chfn
 
 # Install MariaDB
-apt-get update
 apt-get --no-install-recommends install  -y software-properties-common
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 add-apt-repository 'deb [arch=amd64,i386] http://ftp.ddg.lth.se/mariadb/repo/10.1/ubuntu trusty main'
 
 apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 add-apt-repository 'deb http://repo.percona.com/apt trusty main'
-
+apt-get update
 apt-get --no-install-recommends -y upgrade
 
 apt-get --no-install-recommends install -y iproute mariadb-server galera-3 pv iputils-ping net-tools jq percona-xtrabackup socat nmap
